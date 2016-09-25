2ca6919c12038c22d3e51a0d6e52aadea1cbe30f
Alan R. Rogers <rogers@anthro.utah.edu>
2016-09-25 17:41:27 -0600

    Add AA annotations to all files

--- orig/ancall.sh
+++ orig/ancall.sh
@@ -2,4 +2,4 @@ chr=$1
 tsv=$HOME/data/ancall/homo_sapiens/aa${chr}.bgz
 ivcf=vindija-chr${chr}-filtered.vcf.bgz
 out=vindija-chr${chr}-aa.bcf
-bcftools annotate -a ${tsv} -h annots.hdr -O b -c CHROM,POS,AA --output ${out} ${ivcf}
+bcftools annotate -a ${tsv} -h annots.hdr -O b -c CHROM,POS,INFO/AA --output ${out} ${ivcf}
