9d4bd7b5ebfa76b180c8c5c53d69140f9e1bfe23
Alan R. Rogers <rogers@anthro.utah.edu>
2016-09-25 13:45:01 -0600

    Add ancall.sh and annot.hdr. Trying to add AA annotations.

---
+++ orig/ancall.sh
@@ -0,0 +1,5 @@
+chr=$1
+tsv=$HOME/data/ancall/homo_sapiens/aa${chr}.bgz
+ivcf=vindija-chr${chr}-filtered.vcf.bgz
+out=vindija-chr${chr}-aa.bcf
+bcftools annotate -a ${tsv} -h annots.hdr -O b -c CHROM,POS,AA --output ${out} ${ivcf}
