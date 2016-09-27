7257e65256366aa07ccfa772feeddcf7792f4177
Alan R. Rogers <rogers@anthro.utah.edu>
2016-09-27 17:18:36 -0600

    Re-did the ancestral allele calls, using GRCh37 rather than GRCh38.

--- orig/ancall.sh
+++ orig/ancall.sh
@@ -1,5 +1,5 @@
 chr=$1
-tsv=$HOME/data/ancall/homo_sapiens/aa${chr}.bgz
+tsv=$HOME/data/ancall/rel75/aa${chr}.bgz
 ivcf=vindija-chr${chr}-filtered.vcf.bgz
 out=vindija-chr${chr}-aa.bcf
 bcftools annotate -a ${tsv} -h annots.hdr -O b -c CHROM,POS,INFO/AA --output ${out} ${ivcf}
